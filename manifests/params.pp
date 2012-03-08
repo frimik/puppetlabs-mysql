0aa0b516a554e42b56a1ad8a254b74ac0c8473b2
Mikael Fridh <mfridh@marinsoftware.com>
2012-03-08 12:45:34 +0100

    Made socket path adhere to redhat conventions

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,17 +12,19 @@
 # Sample Usage:
 #
 class mysql::params{
-  $socket                   = '/var/run/mysqld/mysqld.sock'
   case $::operatingsystem {
     'centos', 'redhat', 'fedora': {
       $service_name         = 'mysqld'
       $client_package_name  = 'mysql'
+      $socket               = '/var/lib/mysql/mysql.sock'
     }
     'ubuntu', 'debian': {
+      $socket                   = '/var/run/mysqld/mysqld.sock'
       $service_name         = 'mysql'
       $client_package_name  = 'mysql-client'
     }
     default: {
+      $socket                   = '/var/run/mysqld/mysqld.sock'
       $python_package_name  = 'python-mysqldb'
       $ruby_package_name    = 'ruby-mysql'
       $client_package_name  = 'mysql'
